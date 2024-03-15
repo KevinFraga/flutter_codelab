088b33f36aad03f8552a1926f4d3faef72068574
KevinFraga <kevinciolac@gmail.com>
2024-03-14 23:20:16 -0300

    step 7

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,36 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:codelab/generator_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selected = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
-                ),
-              ],
-              selectedIndex: 0,
-              onDestinationSelected: (value) {
-                print('selected: $value');
-              },
+    Widget page;
+    switch (selected) {
+      case 0:
+        page = GeneratorPage();
+        break;
+
+      case 1:
+        page = Placeholder();
+        break;
+
+      default:
+        throw UnimplementedError('Unimplemented');
+    }
+
+    return LayoutBuilder(builder: (context, constrains) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constrains.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selected,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selected = value;
+                  });
+                },
+              ),
             ),
-          ),
-          Expanded(
-            child: GeneratorPage(),
-          )
-        ],
-      ),
-    );
+            Expanded(
+              child: page,
+            )
+          ],
+        ),
+      );
+    });
   }
 }
